544666493036b943a995ebb91e104a789e9aeb87
munjehun <miso1489@gmail.com>
2023-08-26 03:07:33 +0900

    💄 스타일링-1

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, flexDirection: 'row' }}>
-      <View style={styles.container}></View>
-      <StatusBar style="light" />
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Gumi</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
+        </View>
+        <View></View>
+      </View>
     </View>
   );
 }
 
-const styles = (StyleSheet.create = {
+const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: 'mediumpurple' },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: { fontSize: 60, fontWeight: '700' },
+  weather: { flex: 2 },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  temp: { marginTop: 40, fontSize: 178 },
+  desc: { marginTop: -20, fontSize: 60 },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+### React Native Weather App
+
+RN(React Native) 스터디용 날씨 앱 레포지입니다.
